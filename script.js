24320efeb551ad809453fc4b2b1a261caaf713b1
DelaneyRayne <laneyraynedrop@gmail.com>
2025-03-04 15:19:29 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,3 @@
 document.getElementById('surpriseButton').addEventListener('click', function() {
     document.getElementById('surpriseText').style.display = 'block';
 });
-
-// Rotating Banner (Simulated)
-const bannerImages = ['image.banner.jpeg', 'image2.jpg', 'image3.jpg'];
-let currentImage = 0;
-setInterval(() => {
-    currentImage = (currentImage + 1) % bannerImages.length;
-    document.getElementById('bannerImage').src = bannerImages[currentImage];
-}, 3000);
