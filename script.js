5091d6f0e30961c976d47e28dcd68e0e312396d5
DelaneyRayne <laneyraynedrop@gmail.com>
2025-03-03 16:44:14 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+let slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.toggle('active', i === index);
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 3000);  // Change slide every 3 seconds
+showSlide(currentSlide);       // Initial display
