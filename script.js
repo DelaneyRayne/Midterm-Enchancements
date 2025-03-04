14552187704206536212783b01acc461e4884156
DelaneyRayne <laneyraynedrop@gmail.com>
2025-03-04 15:03:43 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-const adImages = [
-    'images/ad1.jpg',
-    'images/ad2.jpg',
-    'images/ad3.jpg'
-];
 
-let currentAdIndex = 0;
-
-function rotateAd() {
-    currentAdIndex = (currentAdIndex + 1) % adImages.length;
-    document.querySelector('.adImage').src = adImages[currentAdIndex];
-}
