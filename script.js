37ac67e7f6ed3aa93cbe1f31c9287fc81aacccbc
DelaneyRayne <laneyraynedrop@gmail.com>
2025-03-04 14:48:16 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-// Add footer year dynamically
-document.addEventListener("DOMContentLoaded", () => {
-    const year = new Date().getFullYear();
-    document.getElementById('footerYear').textContent = `© ${year} Fashion Trends. All rights reserved.`;
-});
+const adImages = [
+    'images/ad1.jpg',
+    'images/ad2.jpg',
+    'images/ad3.jpg'
+];
+
+let currentAdIndex = 0;
+
+function rotateAd() {
+    const adImage = document.querySelector('.ad img');
+    currentAdIndex = (currentAdIndex + 1) % adImages.length;
+    adImage.src = adImages[currentAdIndex];
+}
