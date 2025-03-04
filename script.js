dfb7b69a4475f17c84cd00ca3c58909ce694d4d3
DelaneyRayne <laneyraynedrop@gmail.com>
2025-03-04 15:50:14 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-// Surprise Trend Button Interaction
-document.getElementById('surpriseButton').addEventListener('click', function() {
-    document.getElementById('surpriseText').style.display = 'block';
-});
+
